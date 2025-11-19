f5cad0b065ec24b469eab4d3afc966e826574b15
Onalerona Zwane <onzwanejhb024@student.wethinkcode.co.za>
2025-11-19 09:38:04 +0200

    Handling depencies for component tests

--- src/app/users/user-list/user-list.component.spec.ts
+++ src/app/users/user-list/user-list.component.spec.ts
@@ -1,19 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserListComponent } from './user-list.component';
+import { UserService } from '../user.service';
 
 describe('UserListComponent', () => {
   let component: UserListComponent;
   let fixture: ComponentFixture<UserListComponent>;
+  let userService: UserService;
+  let userServiceSpy: jasmine.Spy
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [UserListComponent]
-    });
-    // Fixture is like a wraaper?
+  beforeEach( async () => {
+    await TestBed.configureTestingModule({
+      declarations: [UserListComponent],
+      providers: [UserService]
+    }).compileComponents()
+
     fixture = TestBed.createComponent(UserListComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    userService = TestBed.inject(UserService)
+    // Spying on our real user service instance
+    userServiceSpy = spyOn(userService, 'getUsers').and.returnValue(of ([
+      {id:1, name: "John"},
+      {id:2, name: "Maria"}
+    ]))
+
+
   });
 
   it('should create', () => {
