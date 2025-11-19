aef8ca549933d704dee91f6b2449d41fdaacf518
Onalerona Zwane <onzwanejhb024@student.wethinkcode.co.za>
2025-11-19 09:19:59 +0200

    Tests in sevrice to assert that service exists and there is more than 1 user

--- src/app/users/user.service.spec.ts
+++ src/app/users/user.service.spec.ts
@@ -1,16 +1,29 @@
 import { TestBed } from '@angular/core/testing';
-
+// Main API to write tests in ANgular is TestBed
 import { UserService } from './user.service';
 
 describe('UserService', () => {
+  
   let service: UserService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(UserService);
-  });
+    // Creatuing testing enviroment
+    TestBed.configureTestingModule({})
+    // Create a new fresh instance of the userservice before each test
+    service = TestBed.inject(UserService)
+  })
 
   it('should be created', () => {
+    // Assert that it exists
     expect(service).toBeTruthy();
-  });
+  })
+
+  it('should getusers', () =>{
+    // Whatever is returned's length should not be empty
+    service.getUsers().subscribe( users  => {
+      expect(users.length).toBeGreaterThan(0)
+    } )
+  })
+
+  
 });
